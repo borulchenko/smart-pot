a844ae61e9bbac063434da3725f943a8b20f7517
Ruslan Borulchenko <rborulchenko@lenovo.com>
2020-12-05 16:29:42 +0200

    SP-11: added startup.sh script in order to start all infrastructure with single command

---
+++ kubernetes/startup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo 'Preparing secrets...'
+kubectl create -f mysql-credentials.yaml
+echo 'Creating mysql db...'
+kubectl create -f mysql-deployment.yaml
+echo 'Creating mqtt broker...'
+kubectl create -f mqtt-deployment.yaml
+echo 'Creating smart-pot-core deployment/service...'
+kubectl create -f smart-pot-core-deployment.yaml
+echo 'Creating smart-pot-client deployment/service...'
+kubectl create -f smart-pot-client-deployment.yaml
